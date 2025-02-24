9dd2b09e0a40fad082e015954a9715ba3ed76ed9
RegL <rennmabuchi@gmail.com>
2025-02-24 12:37:55 +0530

    Updating UI

---
+++ templates/javascr.js
@@ -0,0 +1,24 @@
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const input = event.target;
+    const preview = document.getElementById('imagePreview');
+    const upload = document.getElementById('uploadArea');
+    const imgContainer = document.getElementById('imageContainer');
+  
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+  
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+        upload.style.display = 'none';
+        imgContainer.style.display = 'flex';
+    };
+  
+      reader.readAsDataURL(input.files[0]);
+    } else {
+        preview.src = '#';
+        preview.style.display = 'none';
+        upload.style.display = 'block';
+        imgContainer.style.display = 'none';
+    }
+});
