879af55188187d9a0b86270d68b89fe40793ce42
RegL <rennmabuchi@gmail.com>
2025-02-24 14:59:54 +0530

    More UI design and error handling

--- app.py
+++ app.py
@@ -5,21 +5,26 @@ from PIL import Image
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload():
     if 'image' not in request.files:
-        return 'No file passed'
+        error = 'No files found'
+        return render_template('index.html', error=error)
     
     file = request.files['image']
     
     if(file):
         img = Image.open(io.BytesIO(file.read())).convert('RGB')
         
-        return f'Classification: {ClassifyImage(img)}'
+        classification = ClassifyImage(img)
+        return render_template('index.html', classification=classification)
+    
+    error = 'Invalid file'
+    return render_template('index.html', error=error)
     
 if __name__ == '__main__':
     app.run(debug=True)
